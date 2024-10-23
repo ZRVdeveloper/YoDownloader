313e24525fae193239ee5e887d8b83988da6283d
ZRV <54231560+ZRVdeveloper@users.noreply.github.com>
2024-10-23 12:04:45 +0300

    Update yofun.py

--- yofun.py
+++ yofun.py
@@ -28,12 +28,18 @@ class YoFun():
             print(f'Video  is unavaialable, skipping.')
         else:
             self.stream = self.video.streams.get_highest_resolution()
-            file_name = self.stream.title
-            print(file_name)
-            if os.path.exists(f"{file_name}.mp4"):
+            path = self.stream.title
+            path = str(f'{path.rstrip()}').replace(" ", "_")        
+            path = str(f'{path.rstrip()}').replace("?", "")        
+            path = str(f'{path.rstrip()}').replace("!", "")
+            print(path)
+            if os.path.exists(f"{path}.mp4"):
+                self.parent_win.info.insert('1.0',f"{path} ✔ \n" )
+                self.parent_win.update()
                 return [True]
             else:
-                return [False,file_name]
+                
+                return [False,path]
         
     def convert(self,path):
         video_path = f"vnew.mp4"
@@ -48,7 +54,7 @@ class YoFun():
         d1, d2 = f"del {video_path}", f"del {audio_path}"
         os.system(d1)
         os.system(d2)
-        self.parent_win.info.insert('end', f" path del - ok")
+        self.parent_win.info.insert('1.0', f" path del - ok")
         print('path del - ok')
         return path
     def download (self, url):
@@ -74,6 +80,7 @@ class YoFun():
             self.stream = self.video.streams.get_highest_resolution()        
             self.stream.download()        
             self.parent_win.procent_label.configure(text="✅", fg='green')
+            self.parent_win.info.insert('1.0', f"Відео завантажено \n")
     def download_audio (self,url):
         try:
             self.video = YouTube (url, on_progress_callback=self.v_prog)
@@ -85,20 +92,21 @@ class YoFun():
             self.parent_win.procent_label.configure(text="✅", fg='green')
     def download_list(self, url):
         self.parent_win.geometry("670x550")
-        self.parent_win.info.insert('end', f"Завантажуємо список \n")
+        self.parent_win.info.insert('1.0', f"Завантажуємо список \n")
         v=1
         if v==1:            
             p = Playlist(url)
             self.list_size = len(p.video_urls)
             self.list_download_ok = 0
-            self.parent_win.info.insert('end', f"{self.list_size} у списку \nЗавантаженно: {self.list_download_ok}\n")
+            self.parent_win.info.insert('1.0', f"{self.list_size} у списку. Завантаженно: {self.list_download_ok}\n")
             for vv in p.video_urls:                
                 print(vv)
                 if self.test_name(vv)[0] == False:
                     self.download(vv)
+                    
                 self.list_download_ok += 1
-                self.parent_win.info.insert('end',f"{self.list_size} у списку \nЗавантаженно: {self.list_download_ok}\n")                
-            self.parent_win.info.insert('end',f"{self.list_size} у списку \nЗавантаженно: {self.list_download_ok}\n")
+                self.parent_win.info.insert('1.0',f"{self.list_size} у списку \nЗавантаженно: {self.list_download_ok}\n")                
+            self.parent_win.info.insert('1.0',f"✅ Завантаження списку завершено ✅ \n")
             
     def download_best(self, url):
         try:
